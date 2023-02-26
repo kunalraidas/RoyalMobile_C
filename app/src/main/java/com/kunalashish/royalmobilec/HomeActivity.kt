b474f167e0ededde8dfb09a0fe99e7b463b81c39
kunal <Kunalvirendra2510@outlook.com>
2023-02-26 10:08:20 +0530

    res add

--- app/src/main/java/com/kunalashish/royalmobilec/HomeActivity.kt
+++ app/src/main/java/com/kunalashish/royalmobilec/HomeActivity.kt
@@ -1,17 +1,30 @@
 package com.kunalashish.royalmobilec
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.navigation.NavigationView
 import com.kunalashish.royalmobilec.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private  lateinit var binding: ActivityHomeBinding
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
     }
+
+
+
 }
