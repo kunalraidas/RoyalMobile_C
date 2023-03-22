b6740657c7bf03e8fe1a04f688c32bc3692c945d
Ashish Rajput <ashish8976>
2023-03-22 16:18:15 +0530

    customer profile complete

--- app/src/main/java/com/kunalashish/royalmobilec/activity/MainActivity.kt
+++ app/src/main/java/com/kunalashish/royalmobilec/activity/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.kunalashish.royalmobilec.activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.kunalashish.royalmobilec.R
+import com.kunalashish.royalmobilec.utils.Constant
 
 class MainActivity : AppCompatActivity() {
+
+    var customerEmail : String ? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -17,5 +21,15 @@ class MainActivity : AppCompatActivity() {
         }
         Handler().postDelayed(runnable, 2000)
 
+    }
+
+    private fun isLoggedIn(){
+        val sharedPreferences = getSharedPreferences(Constant.user_pref, Context.MODE_PRIVATE)
+        customerEmail = sharedPreferences.getString(Constant.user_login,null)
+        if (!customerEmail.isNullOrEmpty())
+        {
+            startActivity(Intent(this,HomeActivity::class.java))
+            finish()
+        }
     }
 }
