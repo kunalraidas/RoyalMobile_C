0f2644fb2415c56e31847e4f3db0453f93e4825b
kunal <Kunalvirendra2510@outlook.com>
2023-03-23 20:00:45 +0530

    activity_order_item.xml and details complet

--- app/src/main/java/com/kunalashish/royalmobilec/activity/ProfileFragment.kt
+++ app/src/main/java/com/kunalashish/royalmobilec/activity/ProfileFragment.kt
@@ -30,7 +30,7 @@ class ProfileFragment : Fragment() {
          binding = FragmentProfileBinding.inflate(inflater,container,false)
 
         isLoggedIn()
-        getCustomer()
+        //getCustomer()
 
         binding.settingsBtn.setOnClickListener {
             val intent  = Intent(requireContext(),EditProfileActivity::class.java)
@@ -40,24 +40,24 @@ class ProfileFragment : Fragment() {
         return  binding.root
     }
 
-    private fun getCustomer() {
-        val a = customerEmail?.let {
-            NetworkService.networkInstance.getCustomerDetails(it).enqueue(object : Callback<Customer?> {
-                override fun onResponse(call: Call<Customer?>, response: Response<Customer?>) {
-                    binding.editFirstName.setText(response.body()?.first_name)
-                    binding.editLastName.setText(response.body()?.last_name)
-                    binding.userEmail.text = response.body()?.email
-                    binding.editPhone.setText(response.body()?.phone_no.toString())
-                    binding.editAddress.setText(response.body()?.cust_Address)
-                    binding.username.text = response.body()?.first_name
-                }
-
-                override fun onFailure(call: Call<Customer?>, t: Throwable) {
-                    Toast.makeText(requireContext(), "Fail ${t.message}", Toast.LENGTH_SHORT).show()
-                }
-            })
-        }
-    }
+//    private fun getCustomer() {
+//        val a = customerEmail?.let {
+//            NetworkService.networkInstance.getCustomerDetails(it).enqueue(object : Callback<Customer?> {
+//                override fun onResponse(call: Call<Customer?>, response: Response<Customer?>) {
+//                    binding.editFirstName.setText(response.body()?.first_name)
+//                    binding.editLastName.setText(response.body()?.last_name)
+//                    binding.userEmail.text = response.body()?.email
+//                    binding.editPhone.setText(response.body()?.phone_no.toString())
+//                    binding.editAddress.setText(response.body()?.cust_Address)
+//                    binding.username.text = response.body()?.first_name
+//                }
+//
+//                override fun onFailure(call: Call<Customer?>, t: Throwable) {
+//                    Toast.makeText(requireContext(), "Fail ${t.message}", Toast.LENGTH_SHORT).show()
+//                }
+//            })
+//        }
+//    }
 
 
     private fun isLoggedIn(): String? {
