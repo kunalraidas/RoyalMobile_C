4892ee4623bd34f18e5c824428fa8e8274d34681
kunal <Kunalvirendra2510@outlook.com>
2023-03-26 10:37:49 +0530

    Dashboard background fault solve

--- app/src/main/java/com/kunalashish/royalmobilec/URL.kt
+++ app/src/main/java/com/kunalashish/royalmobilec/URL.kt
@@ -1,6 +1,6 @@
 package com.kunalashish.royalmobilec
 
 object URL {
-    const val BASE_URL = "https://3f2d-103-161-98-144.in.ngrok.io"
+    const val BASE_URL = "https://af46-103-85-11-13.in.ngrok.io"
     const val IMAGE_PATH = BASE_URL+"/storage/images/"
 }
