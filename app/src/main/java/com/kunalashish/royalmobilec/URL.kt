2649c8f5413dbebb9efc58fa26349d7d663f7697
kunal <Kunalvirendra2510@outlook.com>
2023-03-27 11:58:23 +0530

    One singinal NotificationServiceExtension.kt Done

--- app/src/main/java/com/kunalashish/royalmobilec/URL.kt
+++ app/src/main/java/com/kunalashish/royalmobilec/URL.kt
@@ -1,6 +1,6 @@
 package com.kunalashish.royalmobilec
 
 object URL {
-    const val BASE_URL = "https://976f-103-85-11-13.in.ngrok.io"
+    const val BASE_URL = "https://6dac-42-105-173-38.in.ngrok.io"
     const val IMAGE_PATH = BASE_URL+"/storage/images/"
 }
