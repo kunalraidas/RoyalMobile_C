6367d58e424cb55ab29973951e2abac0afe01c89
Ashish Rajput <ashish8976>
2023-03-27 10:54:12 +0530

    change in LoginActivity.kt file

--- app/src/main/java/com/kunalashish/royalmobilec/URL.kt
+++ app/src/main/java/com/kunalashish/royalmobilec/URL.kt
@@ -1,6 +1,6 @@
 package com.kunalashish.royalmobilec
 
 object URL {
-    const val BASE_URL = "https://3f2d-103-161-98-144.in.ngrok.io"
-    const val IMAGE_PATH = BASE_URL+"/storage/images/"
+    const val BASE_URL = "https://c105-42-105-173-38.in.ngrok.io"
+    const val IMAGE_PATH = "$BASE_URL/storage/images/"
 }
