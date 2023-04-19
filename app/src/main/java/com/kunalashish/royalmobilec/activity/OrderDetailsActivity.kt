c9b13464ae850ce2604a197a09430b5775b3f965
kunal <Kunalvirendra2510@outlook.com>
2023-04-19 14:52:37 +0530

    product details change

--- app/src/main/java/com/kunalashish/royalmobilec/activity/OrderDetailsActivity.kt
+++ app/src/main/java/com/kunalashish/royalmobilec/activity/OrderDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.kunalashish.royalmobilec.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kunalashish.royalmobilec.data.product.Order
 import com.kunalashish.royalmobilec.databinding.ActivityOrderDetailsBinding
 
 class OrderDetailsActivity : AppCompatActivity() {
@@ -10,6 +11,15 @@ class OrderDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOrderDetailsBinding.inflate(layoutInflater)
+//        val o = Order()
+//        when(o.orderStatus){
+//            0->{
+//                binding.oDetails.orderedPackedProgress.setProgress(100)
+//            }
+//            1->{
+//                binding.oDetails.orderedPackedProgress.setProgress(100)
+//            }
+//        }
         setContentView(binding.root)
     }
 }
